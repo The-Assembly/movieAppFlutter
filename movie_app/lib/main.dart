fff00564f7727c83d90cafecdc49656439304b54
Aman Sahil <aman.salam2@gmail.com>
2018-05-26 13:53:44 +0400

    Link

--- movie_app/lib/main.dart
+++ movie_app/lib/main.dart
@@ -4,6 +4,31 @@ import 'dart:convert';
 
 void main() => runApp(new MyApp());
 
+class Movie {
+  String poster;
+  String title;
+  String overview;
+  String releaseDate;
+
+  Movie({this.poster,this.title,this.overview,this.releaseDate});
+
+  getTitle(){
+    return this.title;
+  }
+
+  getPoster(){
+    return this.poster;
+  }
+
+  getOverview(){
+    return this.overview;
+  }
+
+  getReleaseDate(){
+    return this.releaseDate;
+  }
+}
+
 class MyApp extends StatelessWidget {
   @override
     Widget build(BuildContext context) {
@@ -29,9 +54,14 @@ class TopMoviesState extends State<TopMovies> {
             title: new Text("What's Hot"),
           ),
           body: new Center(
-            child: new Text("Hello world"),
+            child: _fetchMovies(),
           ),
         ),
       ); 
     }
+
+    List<Movie> _movies = new List();
+    
+    final _apiGateWayUrl = "https://c46ojze5r8.execute-api.us-east-1.amazonaws.com/Testing";
+
 }
